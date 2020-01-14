8ad4e465164845e38177e7acf2118270b2dd21d5
Brendan Jurd <brendan.jurd@geoplex.com.au>
2020-01-14 14:03:00 +1100

    Add basic chess grid layout.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import App from './App.svelte';
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
+  target: document.body,
+  props: {
+    name: 'world',
+    game: '♖♗♘♕♔♗♘♖♙♙♙♙♙♙♙♙                                ♟♟♟♟♟♟♟♟♜♞♝♛♚♝♞♜'
+  }
 });
 
 export default app;
